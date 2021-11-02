1d3444bf136c48ba2be54431db423b33ad6f48c6
teover <43534267+teover@users.noreply.github.com>
2021-11-02 06:01:56 +0100

    Update with urdf and IMU tracking

--- radar_pose_detection/scripts/listener.py
+++ radar_pose_detection/scripts/listener.py
@@ -1,24 +1,58 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 ## Simple talker demo that listens to std_msgs/Strings published 
 ## to the 'chatter' topic
 
 import rospy
 from ti_mmwave_rospkg.msg import RadarScan
+from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Imu
+from tf.transformations import euler_from_quaternion
 
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s')
-    print(data)
+class CommandToJointStateX:
+    def __init__(self):
+        self.joint_name = "base_to_pipe_x"
+        self.joint_state = JointState()
+        self.joint_state.name.append(self.joint_name)
+        self.joint_state.position.append(0.0)
+        self.joint_state.velocity.append(0.0)
+        self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
+        self.command_sub = rospy.Subscriber("/imu_iphone", Imu, self.command_callback, queue_size=1)
 
-def listener():
 
-    rospy.init_node('listener', anonymous=True)
-    
-    rospy.Subscriber('/ti_mmwave/radar_scan', RadarScan, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def command_callback(self, msg):
+        orientation_list = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+        self.joint_state.position[0] = roll
+        self.joint_state.header.stamp = rospy.Time.now()
+        self.joint_pub.publish(self.joint_state)
+
+class CommandToJointStateY:
+    def __init__(self):
+        self.joint_name = "base_to_pipe_y"
+        self.joint_state = JointState()
+        self.joint_state.name.append(self.joint_name)
+        self.joint_state.position.append(0.0)
+        self.joint_state.velocity.append(0.0)
+        self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
+        self.command_sub = rospy.Subscriber("/imu_iphone", Imu,
+                                            self.command_callback, queue_size=1)
+
+    def command_callback(self, msg):
+        orientation_list = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+        self.joint_state.position[0] = pitch
+        self.joint_state.header.stamp = rospy.Time.now()
+        self.joint_pub.publish(self.joint_state)
 
 if __name__ == '__main__':
-    listener()
+    rospy.init_node('command_to_joint_state')
+    command_to_joint_stateX = CommandToJointStateX()
+    command_to_joint_stateY = CommandToJointStateY()
+    rospy.spin()
+
+
+
