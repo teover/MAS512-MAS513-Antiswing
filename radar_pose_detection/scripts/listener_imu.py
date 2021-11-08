037b99717a23b7b37a0cb351f4303e527f31e66e
teover <43534267+teover@users.noreply.github.com>
2021-11-08 20:26:53 +0100

    Commented code

--- radar_pose_detection/scripts/listener_imu.py
+++ radar_pose_detection/scripts/listener_imu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
+## Converts /imu_iphone quaternions to euler angles for "pile-setup.urdf"
+## Sets /joint_states
 
 import rospy
 from ti_mmwave_rospkg.msg import RadarScan
@@ -9,6 +9,7 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
+from math import pi
 
 
 class CommandToJointState:
@@ -17,7 +18,7 @@ class CommandToJointState:
         self.joint_state.name.append("base_to_pipe_x")
         self.joint_state.name.append("base_to_pipe_y")
         self.joint_state.name.append("base_to_pipe_z")
-        print(self.joint_state.name)
+        rospy.loginfo("Publishing joint_states for " + str(self.joint_state.name))
         self.joint_state.position.append([0.0, 0.0, 0.0])
         self.joint_state.velocity.append(0.0)
         self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
@@ -26,12 +27,17 @@ class CommandToJointState:
 
 
     def command_callback(self, msg):
-        orientation_list = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]        
+        # Obtain quaternions xyzw from message
+        orientation_list = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        # Convert quaternions to euler angles  
         (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-        self.joint_state.position = (roll, pitch, yaw)
+        # Log degrees every second
+        rospy.loginfo_throttle(1, "Roll: " + str(roll*(180/pi)) + " Pitch: " + str(pitch*(180/pi)) + " Yaw: " + str(yaw*(180/pi)))
+        # Set joint states from angles
+        self.joint_state.position = (roll, -pitch, yaw)
         self.joint_state.header.stamp = rospy.Time.now()
+        # Publish to topic
         self.joint_pub.publish(self.joint_state)
-
 
 
 if __name__ == '__main__':
--- radar_pose_detection/scripts/listener_radar.py
+++ radar_pose_detection/scripts/listener_radar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
+## Converts /imu_iphone quaternions to euler angles for "pile-setup.urdf"
+## Sets /joint_states
 
 import rospy
 from ti_mmwave_rospkg.msg import RadarScan
@@ -9,46 +9,34 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
+from math import pi
 
 
-class CommandToJointStateX:
+class CommandToJointState:
     def __init__(self):
-        self.joint_name = "base_to_pipe_x"
         self.joint_state = JointState()
-        self.joint_state.name.append(self.joint_name)
-        self.joint_state.position.append(0.0)
+        self.joint_state.name.append("base_to_pipe_x")
+        self.joint_state.name.append("base_to_pipe_y")
+        self.joint_state.name.append("base_to_pipe_z")
+        rospy.loginfo("Publishing joint_states for " + str(self.joint_state.name))
+        self.joint_state.position.append([0.0, 0.0, 0.0])
         self.joint_state.velocity.append(0.0)
         self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
         self.command_sub = rospy.Subscriber("/ti_mmwave/radar_scan", RadarScan, self.command_callback, queue_size=1)
 
 
-    def command_callback(self, msg):
-        print(msg)
-        self.joint_state.position[0] = msg.x
-        self.joint_state.header.stamp = rospy.Time.now()
-        self.joint_pub.publish(self.joint_state)
-
-class CommandToJointStateY:
-    def __init__(self):
-        self.joint_name = "base_to_pipe_y"
-        self.joint_state = JointState()
-        self.joint_state.name.append(self.joint_name)
-        self.joint_state.position.append(0.0)
-        self.joint_state.velocity.append(0.0)
-        self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
-        self.command_sub = rospy.Subscriber("/ti_mmwave/radar_scan", RadarScan, self.command_callback, queue_size=1)
-
 
     def command_callback(self, msg):
-        print(msg)
-        self.joint_state.position[0] = msg.y
+        self.joint_state.position = (msg.x, msg.y, 0)
+        rospy.loginfo("X: " + str(msg.x) + " Y: " + str(msg.y))
         self.joint_state.header.stamp = rospy.Time.now()
+        # Publish to topic
         self.joint_pub.publish(self.joint_state)
+
 
 if __name__ == '__main__':
     rospy.init_node('command_to_joint_state')
-    command_to_joint_stateX = CommandToJointStateX()
-    command_to_joint_stateY = CommandToJointStateY()
+    command_to_joint_stateX = CommandToJointState()
     rospy.spin()
 
 
